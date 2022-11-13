551f4d54f08d95e383eeda1e67ef7f32638cd6fd
super <super@DARKVADOR>
2022-11-13 14:29:35 +0100

    Add project files.

---
+++ Bannerlord.ModuleTestToLearn/SubModule.cs
@@ -0,0 +1,20 @@
+﻿using TaleWorlds.MountAndBlade;
+
+namespace Bannerlord.ModuleTestToLearn {
+    public class SubModule : MBSubModuleBase {
+        protected override void OnSubModuleLoad() {
+            base.OnSubModuleLoad();
+
+        }
+
+        protected override void OnSubModuleUnloaded() {
+            base.OnSubModuleUnloaded();
+
+        }
+
+        protected override void OnBeforeInitialModuleScreenSetAsRoot() {
+            base.OnBeforeInitialModuleScreenSetAsRoot();
+
+        }
+    }
+}
